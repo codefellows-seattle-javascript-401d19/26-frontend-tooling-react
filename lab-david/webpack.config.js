a930679852c394f78304529e2599125062bc4539
David Lindahl <davidalindahl@gmail.com>
2018-01-08 14:05:57 -0800

    added webpack config file per class

---
+++ lab-david/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HTMLPlugin = require('html-webpack-plugin');
+const ExtractPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: "",
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/assets/",
+    library: "MyLibrary",
+  },
+  plugins: [
+    new HTMLPlugin({template: `${__dirname}/src/index.html`}),
+    new ExtractPlugin('bundle.[hash].css'),
+  ],
+  module : {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractPlugin.extract({
+          use: [
+            'css-loader',
+            'resolve-url-loader',
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true,
+                includePaths: [`${__dirname}/src/style`],
+              },
+            },
+          ],
+        }),
+      },
+    ],
+  }
+}
