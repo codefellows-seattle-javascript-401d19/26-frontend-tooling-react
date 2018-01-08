4925db11479cfb5a03286008134a4dc3739e5c1e
Catherine Looper <loopercat@gmail.com>
2018-01-08 15:31:06 -0800

    Enabled webpack script to run build - rendering cowsay and button to page

--- lab-catherine/src/main.js
+++ lab-catherine/src/main.js
@@ -3,4 +3,50 @@
 import './style/main.scss'
 import React from 'react'
 import ReactDom from 'react-dom'
+import cowsay from 'cowsay-browser'
+import faker from 'faker'
 
+class Cow extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <header className='cowsay-hello'>
+        <h1>Generate Cowsay Lorem</h1>
+      </header>
+    );
+  }
+}
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: 'Good Day Cowsay',
+      content: 'What do you say cowsay?',
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState((state) => {
+      return {
+        content: faker.random.words()
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div>
+      <Cow/>
+      <button onClick ={this.handleClick}>clickity click</button>
+      <pre>{cowsay.say({text: this.state.content})}</pre>
+      </div>
+    )
+  }
+}
+
+ReactDom.render(<App/>, document.getElementById('root'));
