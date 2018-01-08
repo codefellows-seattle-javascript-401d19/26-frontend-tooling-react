25ba35cd55b32f2c7b1e82b55632e605a37050af
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2018-01-08 14:24:55 -0800

    write basics of app component and function to generate cowsay text

--- lab-shannon/src/main.js
+++ lab-shannon/src/main.js
@@ -3,10 +3,23 @@
 const main = require('./src/main');
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.state.content = this.props.content;
+  }
+}
+
+function generateCowsay() => {
+
+}
+
+// class App extends React.Component {
   render() {
     return (
       <h1>Generate Cowsay Lorem</h1>
-      <button className="clickMe"></button>
+      <button onClicke={generateCowsay}>Click Me</button>
+      <pre>{App.state.content}</pre>
     );
   }
-}
+// }
