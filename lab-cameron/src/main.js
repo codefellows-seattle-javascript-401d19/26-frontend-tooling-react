1830637617b52b265933e469e1d3da14c33aae49
Cameron Moorehead <hello@cameronmoorehead.com>
2018-01-08 15:28:53 -0800

    added cow with button click to main.js

--- lab-cameron/src/main.js
+++ lab-cameron/src/main.js
@@ -1,1 +1,31 @@
-console.log('what');
+import './style/main.scss';
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import { say } from 'cowsay';
+import faker from 'faker';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { cowText: faker.lorem.words(4) };
+
+    this.buttonClicked = this.buttonClicked.bind(this);
+  }
+
+  buttonClicked() {
+    this.setState({ cowText: faker.lorem.words(4) })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.props.content}</h1>
+        <pre>{say({ text: this.state.cowText })}</pre>
+        <button onClick={this.buttonClicked}>click me</button>
+      </div>
+    )
+  }
+}
+
+ReactDom.render(<App content="Generate Cowsay Lorem"/>, document.getElementById('root'));
