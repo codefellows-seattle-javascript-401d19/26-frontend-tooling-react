195450481520b1659227acc812f4d3f07022326f
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2018-01-08 14:14:34 -0800

    setting up basic page structure

---
+++ lab-shannon/src/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const main = require('./src/main');
+
+class App extends React.Component {
+  render() {
+    return (
+      <h1>Generate Cowsay Lorem</h1>
+      <button className="clickMe"></button>
+    );
+  }
+}
