3925863ec3b28652c1d70710ffa46616ca610f55
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2018-01-08 21:19:31 -0800

    fix bug with scss file; color working properly

--- lab-shannon/webpack.config.js
+++ lab-shannon/webpack.config.js
@@ -6,28 +6,28 @@ const ExtractPlugin = require('extract-text-webpack-plugin');
 module.exports = {
   entry: `${__dirname}/src/main.js`,
   output: {
-    path: `${__dirname}/build`,
     filename: 'bundle.js',
+    path: `${__dirname}/build`,
   },
   plugins: [
     new HTMLPlugin({template: `${__dirname}/src/index.html`}),
-    new ExtractPlugin('bundle.js'),
+    new ExtractPlugin('bundle.css'),
   ],
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: `babel-loader`,
+        loader: 'babel-loader',
       },
       {
         test: /\.scss$/,
         loader: ExtractPlugin.extract({
           use: [
-            `css-loader`,
-            `resolve-url-loader`,
+            'css-loader',
+            'resolve-url-loader',
             {
-              loader: `sass-loader`,
+              loader: 'sass-loader',
               options: {
                 sourceMap: true,
                 includePaths: [`${__dirname}/src/style`],
