09365b47e5bbbd795b6789a008976f69a078fec2
David Lindahl <davidalindahl@gmail.com>
2018-01-11 14:03:27 -0800

    fixed linter and other issues

--- lab-david/src/main.js
+++ lab-david/src/main.js
@@ -1,19 +1,19 @@
 'use strict';
 
-import './style/main.scss'
-import React from 'react'
-import ReactDom from 'react-dom'
-import {say} from 'cowsay'
-import faker from 'faker'
+import './style/main.scss';
+import React from 'react';
+import ReactDom from 'react-dom';
+import {say} from 'cowsay';
+import faker from 'faker';
 
-class Cowsayer extends React.Component {
+class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {};
     this.state.content = say({text: 'click me for mooooore'});
 
-    this.saySomething = this.saySomething.bind(this)
+    this.saySomething = this.saySomething.bind(this);
   }
 
   saySomething() {
@@ -24,13 +24,14 @@ class Cowsayer extends React.Component {
   render() {
     return (
       <div>
-        <button className ="text-s font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-black text-black hover:bg-black hover:text-white" onClick={() => this.saySomething()}> Click me </button>
-        <pre>{this.state.content}
-        </pre>
-    </div>
-    )
+        <button 
+          className ="text-s font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-black text-black hover:bg-black hover:text-white" 
+          onClick={() => this.saySomething()}> Click me </button>
+        <pre>{this.state.content}</pre>
+      </div>
+    );
   }
 }
 
 
-ReactDom.render(<Cowsayer/>, document.getElementById('anchor'))
+ReactDom.render(<App/>, document.getElementById('anchor'));
